f67ec81b38714ca1d914d96c01e2eb2426f7c80d
Ilyas Rima <irilias@gmail.com>
2022-07-29 22:05:42 +0200

    unbinding register & login + setting up register code behind

--- ServiceOutlet/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ServiceOutlet/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,15 +1,66 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace ServiceOutlet.Web.Areas.Identity.Pages.Account
 {
+    [AllowAnonymous]
     public class RegisterModel : PageModel
     {
 
-        public string ReturnUrl { get; set; } = default!;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly ILogger<RegisterModel> logger;
+
+        public RegisterModel(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            ILogger<RegisterModel> logger)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.logger = logger;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } 
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl;
         }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            // TODO : implement the Post method.
+            await Task.CompletedTask;
+            return Page();
+        }
+
     }
+
 }
